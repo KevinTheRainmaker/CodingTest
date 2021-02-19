99bfc291bde7e7fe3e3779eea2b0da795378d3d7
KangbeenKo <sweetrainforyou@gmail.com>
2021-02-19 21:54:00 +0900

    Refactoring 4 (21.02.19)

--- Programmers/4. 문자열 내 p와 y 개수.py
+++ Programmers/4. 문자열 내 p와 y 개수.py
@@ -1,3 +1,4 @@
+# First Code
 def solution(s):
     s = s.lower()
     p=0
@@ -9,3 +10,20 @@ def solution(s):
             y+=1
 
     return p==y
+
+# Refactoring 1 (21.02.19)
+def solution(s):
+    s = s.lower()
+    p, y = (0, 0)
+    for i in s:
+        if i == 'p':
+            p+=1
+        elif i == 'y':
+            y+=1
+
+    return p==y
+
+# Refactoring 2 (21.02.19)
+def solution(s):
+    return s.lower().count('p') == s.lower().count('y')
+    # 문자열 내 특정 문자를 찾아 수를 자동으로 집계해주는 count()함수를 사용
