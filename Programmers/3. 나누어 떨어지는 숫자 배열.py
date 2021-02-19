745ef0cd4c59d8ecbe0e0e48db65faf3534961e8
KangbeenKo <sweetrainforyou@gmail.com>
2021-02-19 21:28:59 +0900

    Refactoring 3

--- Programmers/3. 나누어 떨어지는 숫자 배열.py
+++ Programmers/3. 나누어 떨어지는 숫자 배열.py
@@ -1,8 +1,20 @@
+# def solution(arr, divisor):
+#     answer = []
+#     for i in range(len(arr)):
+#         if arr[i]%divisor==0:
+#             answer.append(arr[i])
+#         else:
+#             answer
+#     if len(answer)==0:
+#         answer.append(-1)
+#     answer.sort()
+#     return answer
+
 def solution(arr, divisor):
     answer = []
-    for i in range(len(arr)):
-        if arr[i]%divisor==0:
-            answer.append(arr[i])
+    for i in arr: # 반복문에 대한 개념이 부족한 상태였기에 했던 실수(?)
+        if i%divisor==0:
+            answer.append(i)
         else:
             answer
     if len(answer)==0:
@@ -10,14 +22,3 @@ def solution(arr, divisor):
     answer.sort()
     return answer
 
-arr = []
-
-while(1):
-    Input = int(input('Enter: '))
-    if Input != -1:
-        arr.append(Input)
-    else:
-        break
-
-divisor = int(input("Divide for what?: "))
-print(solution(arr, divisor))
