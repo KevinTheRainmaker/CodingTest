3d67c75db0082a2aeee34ee5b112c0af815cd41c
KangbeenKo <sweetrainforyou@gmail.com>
2021-02-19 21:34:10 +0900

    Refactoring 1 (21.02.19)

--- Programmers/1. 가운데 글자 가져오기.py
+++ Programmers/1. 가운데 글자 가져오기.py
@@ -1,3 +1,4 @@
+# First Code
 def solution(s):
     answer = ''
     length = len(s)
@@ -6,3 +7,8 @@ def solution(s):
     else:
         answer = s[length//2]
     return answer
+
+# Refactoring 1 (21.02.19)
+def solution(s):
+    return s[(len(s)-1)//2:len(s)//2+1] 
+    # 슬라이싱을 이용하여 s의 글자수가 홀수개인지 짝수개인지에 관계없이 하나의 코드로 완성시켰다
