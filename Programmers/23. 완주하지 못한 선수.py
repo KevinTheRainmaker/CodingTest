07037464bb93d5a858c6b667a221fe3b6a4c4f30
KangbeenKo <sweetrainforyou@gmail.com>
2021-02-22 22:12:59 +0900

    Add 0222

--- Programmers/23. 완주하지 못한 선수.py
+++ Programmers/23. 완주하지 못한 선수.py
@@ -1,3 +1,4 @@
+# 첫 번째 풀이 - 효율성에서 탈락
 def solution(participant, completion):
     for i in completion:
         if i in participant:
@@ -5,4 +6,18 @@ def solution(participant, completion):
     
     return participant[0]
 
-# 답은 모두 정상적으로 도출해내지만 효율성이 걸림돌이 되었다. 이 부분에 대해서 다시 공부해서 통과할 수 있도록 
+# 두 번째 풀이 - 성공
+def solution(p,c):
+    p.sort()
+    c.sort()
+    for par, com in zip(p, c) :
+        if par != com :
+            return par
+    return p[-1]
+
+# 다른 사람의 풀이 - collection 라이브러리의 Count 메서드를 적절히 사용
+import collections
+
+def solution(p, c):
+    answer = collections.Counter(p) - collections.Counter(c)
+    return list(answer.keys())[0]
