1c1f03caa40cab79d339d6c0df4cbe24b0a5c95f
KangbeenKo <sweetrainforyou@gmail.com>
2021-02-26 22:09:10 +0900

    Refactoring - 가독성 up

--- Programmers/31. [1차] 비밀지도.py
+++ Programmers/31. [1차] 비밀지도.py
@@ -1,5 +1,5 @@
 # 첫 번째 풀이 - No Refactoring
-def solution(n, arr1, arr2):
+def solution1(n, arr1, arr2):
     an = [[format(a,'b').zfill(n),format(b,'b').zfill(n)] for a,b in zip(arr1, arr2)]
     swer = []
     sad = ''
@@ -17,15 +17,29 @@ def solution(n, arr1, arr2):
 
 
 # Refactoring - 압축율 max
-def solution(n, arr1, arr2):
+def solution2(n, arr1, arr2):
     # list comprehenshion에 []가 한 번만 있을 경우 앞의 for가 이중 for문에서 바깥쪽 for문
     return [''.join([' ' if int(t[0][i])+int(t[1][i]) == 0 else '#' for t in [[format(a,'b').zfill(n),format(b,'b').zfill(n)]\
      for a,b in zip(arr1, arr2)] for i in range(n)])[n*k:n*(k+1)] for k in range(n)]
 
 
-# Refactoring - 압축율과 효율성의 적당한 줄타기 + 변수명 조정
-def solution(n, arr1, arr2):
+# Refactoring - 의 적당한 줄타기 + 변수명 조정
+def solution3(n, arr1, arr2):
     binary = [[format(a,'b').zfill(n),format(b,'b').zfill(n)] for a,b in zip(arr1, arr2)]
     shop = [' ' if int(t[0][i])+int(t[1][i]) == 0 else '#' for t in binary for i in range(n)]
     answer = [''.join(shop)[n*k:n*(k+1)] for k in range(n)]
     return answer
+
+
+# Refactoring - 가독성 up
+def solution4(n, arr1, arr2):
+    binary = [[format(a,'b').zfill(n),format(b,'b').zfill(n)] for a,b in zip(arr1, arr2)]
+    shop = []
+    for t in binary:
+        for i in range(n):
+            if t[0][i] == '1' or t[1][i] == '1':
+                shop.append('#')
+            else:
+                shop.append(' ')
+
+    return [''.join(shop)[n*k:n*(k+1)] for k in range(n)]
