5071ab21a7305261529ea0a60d7d3d5845b56740
KangbeenKo <sweetrainforyou@gmail.com>
2021-03-04 18:04:44 +0900

    Add 0304(Not Done)

---
+++ Programmers/32. 실패율.py
@@ -0,0 +1,21 @@
+import operator
+
+def solution(N, stages):
+    num = len(stages)
+    if (N+1) in stages:
+        stages.remove((N+1))
+    a = {stage:stages.count(stage) for stage in range(1,N+1)}
+    peoples = [num]
+    people = 0
+    for i in range(N):
+        people += a[i+1]
+        peoples.append(num-people)
+    if num > len(stages):
+        peoples = peoples[:len(stages)-num]
+    failure = dict()
+    for i in range(N):
+        fail = a[i+1]/peoples[i]
+        failure[i+1] = fail
+    almost = sorted(failure.items(), key=operator.itemgetter(1), reverse=True)
+    answer = [thing[0] for thing in almost]
+    return answer
