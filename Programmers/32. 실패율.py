8e01853b43417db4f985e551820c343e2b34d36c
KangbeenKo <sweetrainforyou@gmail.com>
2021-03-04 19:02:10 +0900

    Refactoring PRO32

--- Programmers/32. 실패율.py
+++ Programmers/32. 실패율.py
@@ -1,3 +1,4 @@
+# First Code - RunTime Error
 import operator
 
 def solution(N, stages):
@@ -12,6 +13,25 @@ def solution(N, stages):
         peoples.append(num-people)
     if num > len(stages):
         peoples = peoples[:len(stages)-num]
+    failure = dict()
+    for i in range(N):
+        fail = a[i+1]/peoples[i]
+        failure[i+1] = fail
+    almost = sorted(failure.items(), key=operator.itemgetter(1), reverse=True)
+    answer = [thing[0] for thing in almost]
+    return answer
+
+# Refactoring 1
+import operator
+
+def solution(N, stages):
+    num = len(stages)
+    a = {stage+1:stages.count(stage+1) for stage in range(N)}
+    peoples = [num]
+    people = 0
+    for i in range(N):
+        people += a[i+1]
+        peoples.append(num-people)
     failure = dict()
     for i in range(N):
         fail = a[i+1]/peoples[i]
