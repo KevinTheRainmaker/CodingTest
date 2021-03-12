0630f9d1819394be93ad29b3419aab91db93c62c
KangbeenKo <sweetrainforyou@gmail.com>
2021-03-12 18:11:08 +0900

    Refactoring PRO41

--- Programmers/41. 소수 만들기.py
+++ Programmers/41. 소수 만들기.py
@@ -17,3 +17,20 @@ def solution(nums):
         if isPrime(i) == 0:
             answer += 1
     return answer
+
+
+# Refactoring - isPrime의 리턴값을 0과 1로하여 계산을 간소화
+from itertools import combinations
+
+def isPrime(num):
+    if num == 1:
+        return 0
+    if num %2 == 0:
+        return 0
+    for i in [(k*2)-1 for k in range(2,num//2)]:
+        if num % i == 0:
+            return 0
+    return 1
+    
+def solution(nums):
+    return sum([isPrime(sum(k)) for k in combinations(nums, 3)])
