525834a12244542f3d820a1230457ec4a2957fab
Kangbeen Ko <sweetrainforyou@gmail.com>
2021-11-01 23:36:43 +0900

    feat(BOJ): 10866

---
+++ BOJ/BOJ_10866.py
@@ -0,0 +1,88 @@
+# 정수를 저장하는 덱(Deque)를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+
+# 명령은 총 여덟 가지이다.
+    # push_front X: 정수 X를 덱의 앞에 넣는다.
+    # push_back X: 정수 X를 덱의 뒤에 넣는다.
+    # pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+    # pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+    # size: 덱에 들어있는 정수의 개수를 출력한다.
+    # empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+    # front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+    # back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+# 입력
+    # 첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다. 
+    # 둘째 줄부터 N개의 줄에는 명령이 하나씩 주어진다. 
+    # 주어지는 정수는 1보다 크거나 같고, 100,000보다 작거나 같다. 
+    # 문제에 나와있지 않은 명령이 주어지는 경우는 없다.
+
+# 출력
+    # 출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
+
+''''''
+# 덱 기능 구현
+class Deque:
+    def __init__(self):
+        self.list = []
+    
+    def push_front(self, x):
+        self.list.insert(0, x)
+    
+    def push_back(self, x):
+        self.list.append(x)
+
+    def pop_front(self):
+        if self.size() == 0:
+            return -1
+        return self.list.pop(0)
+
+    def pop_back(self):
+        if self.size() == 0:
+            return -1
+        return self.list.pop(-1)
+
+    def size(self):
+        return len(self.list)
+
+    def empty(self):
+        return 1 if len(self.list) == 0 else 0
+
+    def front(self):
+        if self.size() == 0:
+            return -1
+        return self.list[0]
+
+    def back(self):
+        if self.size() == 0:
+            return -1
+        return self.list[-1]
+
+# 코드
+if __name__ == '__main__':
+    n = int(input())
+    d = Deque()
+    answer = []
+    for _ in range(n):
+        i = input().split()
+        cmd = i[0]
+        if cmd == 'push_front':
+            d.push_front(i[1])
+        elif cmd == 'push_back':
+            d.push_back(i[1])
+        elif cmd == 'pop_front':
+            answer.append(d.pop_front())
+        elif cmd == 'pop_back':
+            answer.append(d.pop_back())
+        elif cmd == 'size':
+            answer.append(d.size())
+        elif cmd == 'empty':
+            answer.append(d.empty())
+        elif cmd == 'front':
+            answer.append(d.front())
+        else:
+            answer.append(d.back())
+
+    for a in answer:
+        print(a)
+
+# 성공 (메모리: 29452 KB, 시간: 480 ms)
